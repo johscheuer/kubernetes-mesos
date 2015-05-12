afe1fbf8608e37a83dba0404ad111ad43f38c81e
Karl Isenberg <karl@mesosphere.io>
2015-05-11 17:00:48 -0700

    Make script/start.sh more robust
    - Extract functions for common code
    - Add connection attempts for etcd & mesos
    - Put logs in /tmp/k8sm-logs
    - etcd starts on localhost, not the public ip

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,69 +1,193 @@
 #!/usr/bin/env bash
 
+# Mac support requires gtimeout & pcregrep (brew install coreutils && brew install pcre)
+
+cmd_exists () {
+    local CMD_NAME="$1"
+    return $(type -p ${CMD_NAME} &> /dev/null)
+}
+
+pid_running () {
+    local CMD_PID="$1"
+    return $(kill -0 ${CMD_PID} &> /dev/null)
+}
+
+TIMEOUT=$(which timeout)
+if ! cmd_exists "timeout"; then
+    if cmd_exists "gtimeout"; then
+        # Mac support
+        TIMEOUT=$(which gtimeout)
+    else
+        echo "timeout or gtimeout command not found"
+        exit 1
+    fi
+fi
+
+ETCD=$(which etcd)
+if ! cmd_exists "etcd"; then
+    echo "etcd command not found"
+    exit 1
+fi
+
+MESOS=$(which mesos-local)
+if ! cmd_exists "mesos-local"; then
+    echo "mesos-local command not found"
+    exit 1
+fi
+
+K8SM="$(pwd)/bin/km"
+if [ ! -e ${K8SM} ]; then
+    echo "./bin/km command not found"
+    exit 1
+fi
+
 set -e
-set -x
 
-export SERVICE_HOST=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
-export MESOS_MASTER=${SERVICE_HOST}:5050
+if cmd_exists "ip"; then
+    export PUBLIC_IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
+elif cmd_exists "ifconfig"; then
+    # Mac support
+    export PUBLIC_IP=$(ifconfig | pcregrep -M -o '^[^\t:]+:([^\n]|\n\t)*status: active' | egrep -o -m 1 '^[^\t:]+' | xargs ipconfig getifaddr)
+else
+    echo "ip or ifconfig command not found"
+    exit 1
+fi
 
-echo "[mesos-cloud]
-    http-client-timeout = 5s
-    state-cache-ttl     = 20s
-" > ./mesos-cloud.conf
+export K8SM_HOST=${PUBLIC_IP}
+export K8SM_PORT=8888
+echo "Kubernetes: ${K8SM_HOST}:${K8SM_PORT}"
 
-etcd \
-  > etcd.log 2>&1 \
+export MESOS_HOST=${PUBLIC_IP}
+export MESOS_PORT=5050
+export MESOS_MASTER=${MESOS_HOST}:${MESOS_PORT}
+echo "Mesos: ${MESOS_MASTER}"
+
+export ETCD_HOST=localhost
+export ETCD_PORT=4001
+export ETCD_URL=http://${ETCD_HOST}:${ETCD_PORT}
+echo "Etcd: ${ETCD_URL}"
+
+K8SM_CONFIG="$(pwd)/mesos-cloud.conf"
+echo "Config: ${K8SM_CONFIG}"
+if [ ! -f "$K8SM_CONFIG" ]; then
+    echo "Writing default config"
+    echo "[mesos-cloud]
+        http-client-timeout = 5s
+        state-cache-ttl     = 20s
+    " > $K8SM_CONFIG
+fi
+
+LOG_DIR="/tmp/k8sm-logs"
+mkdir -p ${LOG_DIR}
+echo "Log Dir: ${LOG_DIR}"
+
+echo "---------------------"
+
+set -e
+
+SUBPROCS=()
+SUBPROC_NAMES=()
+
+await_connection () {
+    export SERVER_HOST="$1"
+    export SERVER_PORT="$2"
+    local SERVER_NAME="$3"
+    echo "Waiting (up to 10s) for ${SERVER_NAME} to accept connections"
+    if ! ${TIMEOUT} 10 bash -c "while ! echo exit | nc -z ${SERVER_HOST} ${SERVER_PORT}; do sleep 0.5; done"; then
+        echo "Timed out"
+        exit 1
+    fi
+}
+
+# Add the pid of a sub-process or exit if it is not running
+add_subproc () {
+    local SUBPROC="$1"
+    local SUBPROC_NAME="$2"
+    if ! ps aux | grep -v grep | grep "${SUBPROC_NAME}" &> /dev/null; then
+        echo "Failed to start ${SUBPROC_NAME}"
+        exit 1
+    fi
+#    echo "Running ${SUBPROC_NAME} (${SUBPROC})"
+    SUBPROCS+=(${SUBPROC})
+    SUBPROC_NAME=${SUBPROC_NAME// /_} # replace spaces with underscores (bash arrays are space deliniated)
+    SUBPROC_NAMES+=(${SUBPROC_NAME})
+}
+
+# Kill subprocesses in reverse order of being started (to avoid log spam)
+kill_subprocs () {
+    echo "Shutting down"
+    set +e
+    for ((i=${#SUBPROCS[@]}-1; i>=0; i--)); do
+        local SUBPROC=${SUBPROCS[i]}
+        local SUBPROC_NAME=${SUBPROC_NAMES[i]}
+        local SUBPROC_NAME=${SUBPROC_NAME//_/ } # replace underscores with spaces
+        if pid_running "${SUBPROC}"; then
+            echo "Killing ${SUBPROC_NAME} (${SUBPROC})"
+            kill ${SUBPROC}
+        fi
+    done
+    set -e
+}
+
+trap 'kill_subprocs' EXIT
+
+echo "Starting etcd"
+${ETCD} \
+  &> ${LOG_DIR}/etcd.log \
   &
-SUBPROC_ETCD="$!"
+add_subproc "$!" "etcd"
+await_connection "${ETCD_HOST}" "${ETCD_PORT}" "etcd"
+echo "---------------------"
 
-mesos-local \
-  > mesos-local.log 2>&1 \
+
+echo "Starting mesos-local"
+${MESOS} \
+  &> ${LOG_DIR}/mesos.log \
   &
-SUBPROC_MESOS="$!"
+add_subproc "$!" "mesos-local"
+await_connection "${MESOS_HOST}" "${MESOS_PORT}" "mesos-local"
+echo "---------------------"
 
-./bin/km apiserver \
-  --address=${SERVICE_HOST} \
-  --etcd_servers=http://${SERVICE_HOST}:4001 \
+
+echo "Starting km apiserver"
+${K8SM} apiserver \
+  --address=${K8SM_HOST} \
+  --etcd_servers=${ETCD_URL} \
   --portal_net=10.10.10.0/24 \
-  --port=8888 \
+  --port=${K8SM_PORT} \
   --cloud_provider=mesos \
-  --cloud_config=./mesos-cloud.conf \
-  > apiserver.log 2>&1 \
+  --cloud_config=${K8SM_CONFIG} \
+  &> ${LOG_DIR}/apiserver.log \
   &
-SUBPROC_API_SERVER="$!"
+add_subproc "$!" "km apiserver"
+await_connection "${K8SM_HOST}" "${K8SM_PORT}" "km apiserver"
+echo "---------------------"
 
-# wait (up to 10 seconds) until the apiserver is responsive
-timeout 10 bash -c "while ! echo exit | nc -z $SERVICE_HOST 8888; do sleep 0.5; done"
 
-./bin/km controller-manager \
-  --master=${SERVICE_HOST}:8888 \
-  --cloud_config=./mesos-cloud.conf \
-  > controller-manager.log 2>&1 \
+echo "Starting km controller-manager"
+${K8SM} controller-manager \
+  --master=${K8SM_HOST}:${K8SM_PORT} \
+  --cloud_config=${K8SM_CONFIG} \
+  &> ${LOG_DIR}/controller-manager.log \
   &
-SUBPROC_CONTROLLER_MANAGER="$!"
+add_subproc "$!" "km controller-manager"
+echo "---------------------"
 
-./bin/km scheduler \
+
+echo "Starting km scheduler"
+${K8SM} scheduler \
   --mesos_master=${MESOS_MASTER} \
-  --address=${SERVICE_HOST} \
-  --etcd_servers=http://${SERVICE_HOST}:4001 \
+  --address=${K8SM_HOST} \
+  --etcd_servers=${ETCD_URL} \
   --mesos_user=root \
-  --api_servers=${SERVICE_HOST}:8888 \
-  > scheduler.log 2>&1 \
+  --api_servers=${K8SM_HOST}:${K8SM_PORT} \
+  &> ${LOG_DIR}/scheduler.log \
   &
-SUBPROC_SCHEDULER="$!"
+add_subproc "$!" "km scheduler"
+echo "---------------------"
 
-trap "
-  echo '--------------------- KILLING PROCESSES'
-  set +e
-  kill $SUBPROC_SCHEDULER
-  kill $SUBPROC_CONTROLLER_MANAGER
-  kill $SUBPROC_API_SERVER
-  kill $SUBPROC_MESOS
-  kill $SUBPROC_ETCD
-" SIGTERM SIGINT
-
-wait $SUBPROC_ETCD
-wait $SUBPROC_MESOS
-wait $SUBPROC_API_SERVER
-wait $SUBPROC_CONTROLLER_MANAGER
-wait $SUBPROC_SCHEDULER
+for subproc in "${SUBPROCS[@]}"; do
+    if pid_running "${subproc}"; then
+        wait ${subproc}
+    fi
+done
